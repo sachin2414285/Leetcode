7470efa39214cb4b2e31cbf2ca6305de720f656a
Sachin Upadhyay <sachinupadhyay2007@gmail.com>
2026-09-18 08:51:41 +0530

    Time: 0 ms (100%), Space: 42.5 MB (9.51%) - LeetHub

---
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int distinctIntegers(int n) {
+//         if(n==1){
+//             return 1;
+//         }
+//         int count=0;
+//         for(int i=1;i<=n;i++){
+//             if(n%i==0){
+//                 count++;
+//             }
+//         }
+//       return count;
+//     }
+// }
+// ess question ke leye yee solution not a best because isme 2 condition ko not take............
+
+
+// ..... main method..........
+
+if(n==1){
+    return 1;
+}
+return n-1;
+    }}
